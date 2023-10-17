d18492120a2e48da33c10fe4207a0ebbfaae5ad6
Aditya Tanwar <86700210+aditya-tanwar@users.noreply.github.com>
2023-10-17 11:12:46 +0530

    Update provider.tf

--- Modules/Rancher-Downstream-Cluster/provider.tf
+++ Modules/Rancher-Downstream-Cluster/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "3.0.1"
+      version = "3.0.1"  // The code is tested for this specific version 
     }
   }
 }
