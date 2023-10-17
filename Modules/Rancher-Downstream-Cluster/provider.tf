24295e3e404895724b307789a8c5ccf5bd858e0d
Aditya Tanwar <86700210+aditya-tanwar@users.noreply.github.com>
2023-10-17 11:12:12 +0530

    Create provider.tf

---
+++ Modules/Rancher-Downstream-Cluster/provider.tf
@@ -0,0 +1,19 @@
+
+
+terraform {
+  required_providers {
+    rancher2 = {
+      source  = "rancher/rancher2"
+      version = "3.0.1"
+    }
+  }
+}
+
+# this block contains the access mechanism to be able to communicate to the rancher using the provider ( accesskeys , secretkeys)
+
+provider "rancher2" {
+  api_url    = var.api_url
+  access_key = var.access_key
+  secret_key = var.secret_key
+  insecure   = var.insecure_connection
+}
