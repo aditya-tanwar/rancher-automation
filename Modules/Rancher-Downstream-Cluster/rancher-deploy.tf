24a0f97ffac44d0c49d026736c073f9d8a82a843
Aditya Tanwar <86700210+aditya-tanwar@users.noreply.github.com>
2023-10-17 11:14:48 +0530

    Update rancher-deploy.tf

--- Modules/Rancher-Downstream-Cluster/rancher-deploy.tf
+++ Modules/Rancher-Downstream-Cluster/rancher-deploy.tf
@@ -1,12 +1,12 @@
 
-# Fetching the vsphere credential metadata from the rancher using the data module. 
+// Fetching the vsphere credential metadata from the rancher using the data module. 
 
 data "rancher2_cloud_credential" "auth" {
   name = var.cloud_credential
 }
 
 
-# Creating a cluster using the node pools 
+// Creating a cluster using the node pools 
 
 resource "rancher2_cluster_v2" "foo-rke2" {
   name                                     = var.cluster_name
@@ -14,7 +14,7 @@ resource "rancher2_cluster_v2" "foo-rke2" {
   enable_network_policy                    = var.enable_network_policy
   default_cluster_role_for_project_members = var.default_cluster_role_for_project_members
 
-# This is section for configuring the authorized endpoint ( ACE )
+// This is section for configuring the authorized endpoint ( ACE )
 
   local_auth_endpoint {
     ca_certs = file("./test.pem", )
@@ -22,7 +22,7 @@ resource "rancher2_cluster_v2" "foo-rke2" {
     fqdn     = "rancher.rancherlab02.rancher.prgx.com:6443"
   }
 
-# This section below defines the lifecycle of the authorized endpoint ( ACE ) to protect the drifting of its configuration
+// This section below defines the lifecycle of the authorized endpoint ( ACE ) to protect the drifting of its configuration
   
   lifecycle {
     ignore_changes = [
@@ -32,27 +32,27 @@ resource "rancher2_cluster_v2" "foo-rke2" {
 
   rke_config {
 
-    # Creating the machine global config in the rke_config block 
+    // Creating the machine global config in the rke_config block 
 
     machine_global_config = <<EOF
       cni: ${var.cni}
       disable-kube-proxy: false
       etcd-expose-metrics: false
     
-    # The below setting allows a node to be able to schedule 250 pods 
+    // The below setting allows a node to be able to schedule 250 pods 
     
       kubelet-arg:
         - max-pods=250
     EOF
 
-    # This section is for etcd backups scheduling using cron jobs  
+    // This section is for etcd backups scheduling using cron jobs  
     
     etcd {
-      snapshot_schedule_cron = "0 */24 * * *" # cron format
+      snapshot_schedule_cron = "0 */24 * * *"   // cron format
       snapshot_retention = 5
     }
 
-# Creating dynamic machine pools 
+// Creating dynamic machine pools 
 
     dynamic "machine_pools" {
       for_each = var.node
@@ -64,7 +64,7 @@ resource "rancher2_cluster_v2" "foo-rke2" {
         quantity                     = machine_pools.value.quantity
         worker_role                  = machine_pools.key != "ctl_plane" ? true : false
         machine_labels  = (machine_pools.key == "storage") || (machine_pools.key == "ingress")  ? "${lookup(var.labels, machine_pools.key, )}" : null
-        dynamic "taints"{ # Adding the taints dynamically to the nodes ( ingress / storage ) 
+        dynamic "taints"{ // Adding the taints dynamically to the nodes ( ingress / storage ) 
           for_each = (machine_pools.key == "storage") || (machine_pools.key == "ingress") ?  [1] : []
           content {
                 key = "dedicated"
@@ -76,12 +76,12 @@ resource "rancher2_cluster_v2" "foo-rke2" {
           kind = rancher2_machine_config_v2.machineconfig[machine_pools.key].kind
           name = replace(rancher2_machine_config_v2.machineconfig[machine_pools.key].name, "_", "-")
         }
-      } # End of dynamic for_each content
-    }   # End of machine_pools
+      } // End of dynamic for_each content
+    }   // End of machine_pools
   }
 }
 
-# Using the locals to split the network name to be used in the machine config vapp section
+// Using the locals to split the network name to be used in the machine config vapp section
 
 locals {
     filter_out = split("/",var.vsphere_env.vm_network[0])[3]
@@ -92,16 +92,16 @@ output "extracted_content" {
 }
 
 
-# Creating a machine config to be used while creating the cluster
+// Creating a machine config to be used while creating the cluster
 
 resource "rancher2_machine_config_v2" "machineconfig" {
   for_each      = var.node
   generate_name = "${each.value.name}-config"
 
   vsphere_config {
-    cfgparam      = ["disk.enableUUID=TRUE"] # Disk UUID is Required for vSphere Storage Provider ( mandatory for the cluster to work )
+    cfgparam      = ["disk.enableUUID=TRUE"] // Disk UUID is Required for vSphere Storage Provider ( mandatory for the cluster to work )
     clone_from    = var.vsphere_env.cloud_image_name
-    cloud_config  = file("/root/aditya/Modules/lab_rancher/cloud.yaml", ) # This is basically the path where the cloud.yaml is stored 
+    cloud_config  = file("/root/aditya/Modules/lab_rancher/cloud.yaml", ) // This is basically the path where the cloud.yaml is stored 
     cpu_count     = each.value.cpu
     creation_type = "template"
     datacenter    = var.vsphere_env.datacenter
@@ -112,7 +112,7 @@ resource "rancher2_machine_config_v2" "machineconfig" {
     folder        = var.vsphere_env.folder
     pool          = var.vsphere_env.pool
 
-    # The section below defines the vapp configuration for the cluster 
+    // The section below defines the vapp configuration for the cluster 
 
     vapp_ip_allocation_policy = "fixedAllocated"
     vapp_ip_protocol=  "IPv4"
@@ -125,7 +125,7 @@ resource "rancher2_machine_config_v2" "machineconfig" {
     vapp_transport = "com.vmware.guestInfo"
   }
 
-  # This is a lifecycle management to prevent automatic drifts in the cloud_config ( LINE 104 ) 
+  // This is a lifecycle management to prevent automatic drifts in the cloud_config ( LINE 104 ) 
   lifecycle {
     ignore_changes = [
         vsphere_config[0].cloud_config,
